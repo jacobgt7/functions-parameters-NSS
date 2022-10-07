106a5d07097d4529c9447aab47153bb33b88b091
Jacob Thomas <jacgretho@gmail.com>
2022-10-07 09:46:26 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const quarterValue = (num) => {
+    // Define a parameter that will hold the value of the original number.
+    // Divide that number by 4
+    return num / 4
+
+}
+
+// Invoke the function and store the return value
+
+const quarter88 = quarterValue(88)
+
+// Log the return value
+console.log(quarter88)
